c2c2b13bd99168a575cbdc773bb519ac0002b11f
heindricx <heindricx@github.com>
2026-06-20 01:07:44 +0800

    fix: Update Supabase project credentials for V2

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://ppukvumeefcmbwidtzuj.supabase.co'
-const supabaseKey = 'sb_publishable_QreVd28xHQ1YctZ0Hupa7g_Wzxnoz5c'
+const supabaseUrl = 'https://oallytrmvxphbhyddjht.supabase.co'
+const supabaseKey = 'sb_publishable_QqcKp1YHms9CNm8I0xXUPA_i-Gog3jR'
 export const supabase = createClient(supabaseUrl, supabaseKey)
